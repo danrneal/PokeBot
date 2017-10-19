e212c983a572e91dce74c177864ae252ec7940e3
Dan Neal <dan.r.neal@gmail.com>
2017-10-18 23:21:57 -0400

    processing DMs

--- PokeBot/processing.py
+++ PokeBot/processing.py
@@ -3,7 +3,7 @@
 
 import logging
 import asyncio
-from datetime import datetime, timedelta
+from datetime import datetime
 from .utils import get_args, Dicts
 
 log = logging.getLogger('processing')
@@ -12,8 +12,13 @@ args = get_args()
 
 async def in_q(client, bot_number):
     while True:
-        if not dicts.bots[bot_number]['in_queue'].empty():
-            obj = dicts.bots[bot_number]['in_queue'].get()
+        if not Dicts.bots[bot_number]['in_queue'].empty():
+            if Dicts.bots[bot_number]['in_queue'].qsize() > 300:
+                log.warning((
+                    "Bot queue length is at {}... this may be causing a " +
+                    "delay in notifications, consider adding more bots."
+                ).format(dicts.bots[bot_number]['in_queue'].qsize()))
+            obj = Dicts.bots[bot_number]['in_queue'].get()
             try:
                 if obj['type'] == "pokemon":
                     process_pokemon(client, bot_number, obj)
@@ -86,50 +91,101 @@ def check_egg_filter(settings, egg):
 def process_pokemon(client, bot_number, pkmn):
     user_ids = []
     pkmn_id = pkmn['pkmn_id']
-    for user_id in dicts.bots[bot_number]['filters']:
-        if (dicts.bots[bot_number]['filters'][user_id] is True or
-            pkmn['geofence'] not in dicts.bots[bot_number]['filters'][user_id][
-                'areas'] or
-            dicts.bots[bot_number]['filters'][user_id]['pokemon_setttings'][
-            'enabled'] is False or
-            pkmn_id not in dicts.bots[bot_number]['filters'][user_id][
-                'pokemon_setttings']['filters']):
-            break
-        filters = dicts.bots[bot_number]['filters'][user_id][
-            'pokemon_setttings']['filters'][pkmn_id]
+    lat, lng = pkmn['lat'], pkmn['lng']
+    name = pkmn['pkmn']
+    for user_id in Dicts.bots[bot_number]['filters']:
+        if (Dicts.bots[bot_number]['filters'][user_id]['paused'] is True or
+            Dicts.bots[bot_number]['pokemon_setttings'][user_id][
+                'enabled'] is False or
+            pkmn_id not in Dicts.bots[bot_number]['pokemon_setttings'][
+                user_id]['filters']):
+            continue
+        filters = Dicts.bots[bot_number]['pokemon_setttings']['filters'][
+            pkmn_id]
         passed = check_pokemon_filter(filters, pkmn)
         if not passed:
-            break
+            continue
+        if (len(Dicts.bots[bot_number]['filters'][user_id]['areas']) > 0 and
+            pkmn['geofence'] not in Dicts.bots[bot_number]['filters'][user_id][
+                'areas']):
+            continue
         user_ids.append(user_id)
-    log.info("DM notification has been triggered!")
     if len(user_ids) > 0:
-        log.info("{} DM notification has been triggered!".format(pkmn['name']))
-        dicts.bots[bot_number]['alarm'].pokemon_alert(
+        if Dicts.bots[bot_number]['loc_service']:
+            Dicts.bots[bot_number]['loc_service'].add_optional_arguments(
+                [lat, lng], pkmn)
+        log.info("{} DM notification has been triggered!".format(name))
+        Dicts.bots[bot_number]['alarm'].pokemon_alert(
             client, bot_number, pkmn, user_ids)
 
 
+def process_egg(client, bot_number, egg):
+    user_ids = []
+    lat, lng = egg['lat'], egg['lng']
+    gym_id = egg['id']
+    for user_id in Dicts.bots[bot_number]['filters']:
+        if (Dicts.bots[bot_number]['filters'][user_id]['paused'] is True or
+            Dicts.bots[bot_number]['egg_settings'][user_id][
+                'enabled'] is False):
+            continue
+        passed = check_egg_filter(
+            Dicts.bots[bot_number]['egg_settings'][user_id], egg)
+        if not passed:
+            continue
+        if (len(Dicts.bots[bot_number]['filters'][user_id]['areas']) > 0 and
+            egg['geofence'] not in Dicts.bots[bot_number]['filters'][user_id][
+                'areas']):
+            continue
+        user_ids.append(user_id)
+    if len(user_ids) > 0:
+        if Dicts.bots[bot_number]['loc_service']:
+            Dicts.bots[bot_number]['loc_service'].add_optional_arguments(
+                [lat, lng], egg)
+        log.info("Egg ({}) notification has been triggered!".format(gym_id))
+        Dicts.bots[bot_number]['alarm'].raid_egg_alert(
+            client, bot_number, egg, user_ids)
+
+
+def process_raid(client, bot_number, raid):
+    user_ids = []
+    pkmn_id = raid['pkmn_id']
+    lat, lng = raid['lat'], raid['lng']
+    gym_id = raid['id']
+    for user_id in Dicts.bots[bot_number]['filters']:
+        if (Dicts.bots[bot_number]['filters'][user_id]['paused'] is True or
+            Dicts.bots[bot_number]['raid_setttings'][user_id][
+                'enabled'] is False or
+            pkmn_id not in Dicts.bots[bot_number]['raid_setttings'][user_id][
+                'filters']):
+            continue
+        if (len(Dicts.bots[bot_number]['filters'][user_id]['areas']) > 0 and
+            pkmn['geofence'] not in Dicts.bots[bot_number]['filters'][user_id][
+                'areas']):
+            continue
+        user_ids.append(user_id)
+    if len(user_ids) > 0:
+        if Dicts.bots[bot_number]['loc_service']:
+            Dicts.bots[bot_number]['loc_service'].add_optional_arguments(
+                [lat, lng], raid)
+        log.info("Raid ({}) notification has been triggered!".format(gym_id))
+        Dicts.bots[bot_number]['alarm'].raid_alert(
+            client, bot_number, raid, user_ids)
+
+
 async def out_q(bot_number):
-    while not dicts.bots[bot_number]['out_queue'].empty():
-        if dicts.bots[bot_number]['out_queue'].qsize() > 300:
-            log.warning((
-                "Out queue length is at {}... this may be causing a delay " +
-                "in notifications, consider adding more bots."
-            ).format(dicts.bots[bot_number]['out_queue'].qsize()))
-        while len(dicts.bots[bot_number]['timestamps']) >= 120:
-            if (datetime.utcnow() - dicts.bots[bot_number]['timestamps'][
+    while not Dicts.bots[bot_number]['out_queue'].empty():
+        while len(Dicts.bots[bot_number]['timestamps']) >= 120:
+            if (datetime.utcnow() - Dicts.bots[bot_number]['timestamps'][
                     0]).total_seconds() > 60:
-                dicts.bots[bot_number]['timestamps'].pop(0)
+                Dicts.bots[bot_number]['timestamps'].pop(0)
             else:
                 await asyncio.sleep(0.5)
-        msg_params = await dicts.bots[bot_number]['out_queue'].get()
+        msg_params = await Dicts.bots[bot_number]['out_queue'].get()
         await msg_params[2]['destination'].send(
             msg_params[2].get('msg'),
             embed=msg_params[2].get('embed')
         )
         log.info('Sent msg to {}'.format(msg_params[2]['destination'].name))
-        dicts.bots[bot_number]['timestamps'].append(datetime.utcnow())
-    dicts.bots[bot_number]['count'] = 0
+        Dicts.bots[bot_number]['timestamps'].append(datetime.utcnow())
+    Dicts.bots[bot_number]['count'] = 0
     await asyncio.sleep(0.5)
-
-
-
